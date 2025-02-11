186c23469cf8c100ea10074cacfa4baae3e2411f
Khaerul743 <khaerullutfi028@gmail.com>
2025-02-11 20:03:57 +0700

    menambahkan cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const productRoutes = require('./routes/productRoutes') 
 const authRoutes = require('./routes/authRoutes')
@@ -11,6 +12,7 @@ const cookieParser = require('cookie-parser')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors())
 app.use('/product',productRoutes);
 app.use('/auth',authRoutes)
 app.use('/order',orderRoutes)
