507094a92965d73ba102300a182fb9dc75adb988
Khaerul743 <khaerullutfi028@gmail.com>
2025-02-15 11:10:36 +0700

    menambahkan JWT dan role authentikasi di productRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const {verifyToken,authorizationRoles} = require('../utils/verifyToken')
 const {listProduct,addProduct,updateProduct,deleteProduct,detailProduct} = require('../controller/productController')
 
-router.get('/',listProduct)
-router.get('/:id',detailProduct)
-router.post('/',addProduct);
-router.put('/:id',updateProduct);
-router.delete('/:id',deleteProduct);
+router.get('/',verifyToken,authorizationRoles("customer","employee","admin"),listProduct)
+router.get('/:id',verifyToken,authorizationRoles("customer","employee","admin"),detailProduct)
+router.post('/',verifyToken,authorizationRoles("admin","employee"),addProduct);
+router.put('/:id',verifyToken,authorizationRoles("admin","employee"),updateProduct);
+router.delete('/:id',verifyToken,authorizationRoles("admin","employee"),deleteProduct);
 
 module.exports = router;
